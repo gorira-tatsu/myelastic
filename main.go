ff83b51e830dbdf475a7e217a9c8f2bfb0729e01
gorira <tatsuhiko.shigoto@gmail.com>
2023-11-29 20:40:46 +0900

    receive json

--- main.go
+++ main.go
@@ -1,15 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hellow world, %s", request.URL.Path[1:])
+var datas []QueryData
+
+type QueryData struct {
+	Text string `json:"text"`
+}
+
+func entry(writer http.ResponseWriter, request *http.Request) {
+	var data QueryData
+
+	err := json.NewDecoder(request.Body).Decode(&data)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if data.Text == "" {
+		http.Error(writer, "Text field is empty", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received: %+v\n", data)
+	datas = append(datas, data)
+
+	writer.Header().Set("Content-Type", "application/json")
+}
+
+func search(writer http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(writer, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	search_word := string(body)
+
+	for _, data := range datas {
+		if search_word == data.Text {
+			fmt.Fprintf(writer, "yes")
+		}
+	}
+
+	writer.Header().Set("Content-Type", "text/plain")
+}
+
+func show(writer http.ResponseWriter, request *http.Request) {
+	if datas != nil {
+		fmt.Fprintln(writer, datas)
+	} else {
+		fmt.Fprintf(writer, "none")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/entry", entry)
+	http.HandleFunc("/show", show)
+	http.HandleFunc("/search", search)
 	http.ListenAndServe(":8080", nil)
 }
